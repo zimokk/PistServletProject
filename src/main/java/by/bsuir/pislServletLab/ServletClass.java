851af65ffd2963b8d52defe1e3c1baa01d1750ec
Dmitry <zimokk1@gmail.com>
2016-09-08 19:42:28 +0200

    'fixes'

--- src/main/java/by/bsuir/pislServletLab/ServletClass.java
+++ src/main/java/by/bsuir/pislServletLab/ServletClass.java
@@ -1,12 +1,13 @@
 package by.bsuir.pislServletLab;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
+@WebServlet("/")
 public class ServletClass extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
