3981eeafc504d6ba2aa26791ce4070236b65ba4b
Dmitry <zimokk1@gmail.com>
2016-09-10 13:36:26 +0200

    Edited links, added servlet post-method

--- src/main/java/by/bsuir/pislServletLab/ServletClass.java
+++ src/main/java/by/bsuir/pislServletLab/ServletClass.java
@@ -7,12 +7,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/")
+@WebServlet("/index")
 public class ServletClass extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        System.out.println("get get req");
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
 
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+            System.out.println("get post req");
         req.getRequestDispatcher("index.jsp").forward(req, resp);
     }
 
